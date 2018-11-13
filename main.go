290848ed79a00ccf411982006092deee2e9cbb36
Jovi <redzjovi@gmail.com>
2018-11-13 17:39:49 +0700

    [Route] add hotel.

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "gin2018/routes"
+    "github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	router := gin.Default()
+
+    routes.Api{}.Include(router)
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
---
+++ routes/api.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+    "gin2018/app/http/controllers"
+    "github.com/gin-gonic/gin"
+)
+
+type Api struct {}
+
+func (Api) Include(router *gin.Engine) {
+    router.GET("/api/hotel", controllers.HotelController{}.Index)
+    router.POST("/api/hotel", controllers.HotelController{}.Store)
+    router.GET("/api/hotel/:id", controllers.HotelController{}.Show)
+    router.PUT("/api/hotel/:id", controllers.HotelController{}.Update)
+    router.DELETE("/api/hotel/:id", controllers.HotelController{}.Destroy)
+}
